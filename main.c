55337bdaae9f0b48aa130634a1c22c0f1ff00537
diandrade <contato.andradediego@gmail.com>
2026-07-17 18:45:51 -0300

    feat: Ajustes na formatação

--- main.c
+++ main.c
@@ -1,73 +1,67 @@
 #include "push_swap.h"
 
-int check_numbers(char **dst)
+int	check_numbers(char **dst)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    if (!dst || !dst[0])
-        return (0);
-
-    i = 0;
-    while(dst[i] != NULL)
-    {
-        j = 0;
-        if (dst[i][j] == '-' || dst[i][j] == '+')
-            j++;
-
-        if (!dst[i][j]) 
-            return (0);
-
-        while (dst[i][j])
-        {
-            if (ft_isdigit(dst[i][j]))
-                j++;
-            else
-                return (0);
-        }
-        i++;
-    }
-    return (1);
+	if (!dst || !dst[0])
+		return (0);
+	i = 0;
+	while (dst[i] != NULL)
+	{
+		j = 0;
+		if (dst[i][j] == '-' || dst[i][j] == '+')
+			j++;
+		if (!dst[i][j])
+			return (0);
+		while (dst[i][j])
+		{
+			if (ft_isdigit(dst[i][j]))
+				j++;
+			else
+				return (0);
+		}
+		i++;
+	}
+	return (1);
 }
 
-char **parse_args(int argc, char **argv)
+char	**parse_args(int argc, char **argv)
 {
-    static int i;
-    char **dst;
+	static int	i;
+	char		**dst;
 
-    if (!i)
-        i = 1;
-
-    dst = NULL;
-    if (i < argc)
-    {
-        dst = ft_split(argv[i++], ' ');
-        
-        if (check_numbers(dst))
-            return (dst);
-        
-        write(2, "Error\n", 6);
-        exit(1);
-    }
-    return (NULL);
+	if (!i)
+		i = 1;
+	dst = NULL;
+	if (i < argc)
+	{
+		dst = ft_split(argv[i++], ' ');
+		if (check_numbers(dst))
+			return (dst);
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	return (NULL);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack *stack_a;
-    char    **current_args;
+	t_stack *stack_a;
+	char **current_args;
 
-    if (argc > 1)
-    {
-        stack_a = NULL;
-        
-        while ((current_args = parse_args(argc, argv)) != NULL)
-        {
-            append_node(current_args, &stack_a);
-        }
-    
-        print_stack(stack_a);
-        return (0);
-    }
-    return (1);
+	if (argc > 1)
+	{
+		stack_a = NULL;
+
+		while ((current_args = parse_args(argc, argv)) != NULL)
+		{
+			append_node(current_args, &stack_a);
+		}
+
+		print_stack(stack_a);
+		return (0);
+	}
+	return (1);
 }
